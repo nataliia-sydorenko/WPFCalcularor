8601d959cb31cc3552bb180e028e86802b882349
nataliia-sydorenko <sydorenkonataly@gmail.com>
2019-07-02 21:35:51 +0300

    first commit

---
+++ Model/CalculatingModel.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class CalculatingModel
+    {
+        private string result;
+
+        public string FirstOperand { get; set; }
+        public string SecondOperand { get; set; }
+        public string Operation { get; set; }
+        public string Result { get { return result; } }
+
+        public CalculatingModel()
+        {
+            FirstOperand = string.Empty;
+            SecondOperand = string.Empty;
+            Operation = string.Empty;
+            result = string.Empty;
+        }
+
+        public void CalculateResult()
+        {
+            ValidateData();
+
+            try
+            {
+                switch (Operation)
+                {
+                    case ("+"):
+                        result = (Convert.ToDouble(FirstOperand) + Convert.ToDouble(SecondOperand)).ToString();
+                        break;
+
+                    case ("-"):
+                        result = (Convert.ToDouble(FirstOperand) - Convert.ToDouble(SecondOperand)).ToString();
+                        break;
+
+                    case ("*"):
+                        result = (Convert.ToDouble(FirstOperand) * Convert.ToDouble(SecondOperand)).ToString();
+                        break;
+
+                    case ("/"):
+                        result = (Convert.ToDouble(FirstOperand) / Convert.ToDouble(SecondOperand)).ToString();
+                        break;
+                }
+            }
+            catch (Exception)
+            {
+                result = "Calculating Error";
+                throw;
+            }
+        }
+
+        private void ValidateOperand(string operand)
+        {
+            try
+            {
+                Convert.ToDouble(operand);
+            }
+            catch (Exception)
+            {
+                result = "Invalid number: " + operand;
+                throw;
+            }
+        }
+
+        private void ValidateOperation(string operation)
+        {
+            switch (operation)
+            {
+                case "/":
+                case "*":
+                case "-":
+                case "+":
+                    break;
+                default:
+                    result = "Unknown operation: " + operation;
+                    throw new ArgumentException("Unknown Operation: " + operation, "operation");
+            }
+        }
+
+        private void ValidateData()
+        {
+            switch (Operation)
+            {
+                case "/":
+                case "*":
+                case "-":
+                case "+":
+                    ValidateOperand(FirstOperand);
+                    ValidateOperand(SecondOperand);
+                    break;
+                default:
+                    result = "Unknown operation: " + Operation;
+                    throw new ArgumentException("Unknown Operation: " + Operation, "operation");
+            }
+
+        }
+    }
+}
---
+++ ViewModel/CalculatorViewModel.cs
@@ -0,0 +1,176 @@
+﻿using Model;
+using System;
+using System.Windows.Input;
+
+namespace ViewModel
+{
+    public class CalculatorViewModel : ViewModelBase
+    {
+
+        private CalculatingModel calculation;
+
+        private string lastOperation;
+        private bool newDisplayRequired = false;
+        private string display;
+
+
+        public CalculatorViewModel()
+        {
+            this.calculation = new CalculatingModel();
+            this.display = "0";
+            this.FirstOperand = string.Empty;
+            this.SecondOperand = string.Empty;
+            this.Operation = string.Empty;
+            this.lastOperation = string.Empty;
+        }
+
+        private RelayCommand digitButtonPressCommand;
+        private RelayCommand operationButtonPressCommand;
+
+        public string FirstOperand
+        {
+            get { return calculation.FirstOperand; }
+            set { calculation.FirstOperand = value; }
+        }
+
+        public string SecondOperand
+        {
+            get { return calculation.SecondOperand; }
+            set { calculation.SecondOperand = value; }
+        }
+
+        public string Operation
+        {
+            get { return calculation.Operation; }
+            set { calculation.Operation = value; }
+        }
+
+        public string LastOperation
+        {
+            get { return lastOperation; }
+            set { lastOperation = value; }
+        }
+
+        public string Result
+        {
+            get { return calculation.Result; }
+        }
+
+        public string Display
+        {
+            get { return display; }
+            set
+            {
+                display = value;
+                OnPropertyChanged("Display");
+            }
+        }
+
+        public ICommand OperationButtonPressCommand
+        {
+            get
+            {
+                if (operationButtonPressCommand == null)
+                {
+                    operationButtonPressCommand = new RelayCommand(OperationButtonPress);
+                }
+                return operationButtonPressCommand;
+            }
+        }
+
+        public ICommand DigitButtonPressCommand
+        {
+            get
+            {
+                if (digitButtonPressCommand == null)
+                {
+                    digitButtonPressCommand = new RelayCommand(DigitButtonPress);
+                }
+                return digitButtonPressCommand;
+            }
+        }
+
+        public void DigitButtonPress(object button)
+        {
+            switch (button)
+            {
+                case "C":
+                    Display = "0";
+                    FirstOperand = string.Empty;
+                    SecondOperand = string.Empty;
+                    Operation = string.Empty;
+                    LastOperation = string.Empty;
+                    break;
+                case "+/-":
+                    if (display.Contains("-") || display == "0")
+                    {
+                        Display = display.Remove(display.IndexOf("-"), 1);
+                    }
+                    else Display = "-" + display;
+                    break;
+                case ".":
+                    if (newDisplayRequired)
+                    {
+                        Display = "0,";
+                    }
+                    else
+                    {
+                        if (!display.Contains("."))
+                        {
+                            Display = display + ",";
+                        }
+                    }
+                    break;
+                case "%":
+                    if (FirstOperand == string.Empty)
+                    {
+                        Display = "0";
+                    }
+                    else
+                    {
+                        Display = ((Convert.ToDouble(FirstOperand)) / 100 * (Convert.ToDouble(display))).ToString();
+                        display = Display;
+                    }
+                    break;
+                default:
+                    if (display == "0" || newDisplayRequired)
+                        Display = button.ToString();
+                    else
+                        Display = display + button;
+                    break;
+            }
+            newDisplayRequired = false;
+        }
+
+
+        public void OperationButtonPress(object operation)
+        {
+            try
+            {
+                if (FirstOperand == string.Empty || LastOperation == "=")
+                {
+                    FirstOperand = display;
+                    LastOperation = operation.ToString();
+                }
+                else
+                {
+                    SecondOperand = display;
+                    Operation = lastOperation;
+                    calculation.CalculateResult();
+
+                    LastOperation = operation.ToString();
+                    Display = Result;
+                    FirstOperand = display;
+                }
+                newDisplayRequired = true;
+            }
+            catch (Exception)
+            {
+                Display = Result == string.Empty ? "Error has occured" : Result;
+            }
+        }
+
+    }
+}
+
+
